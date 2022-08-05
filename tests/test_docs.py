bf619c435f2eee3a20f5ed82c45579ce69dd4168
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 02:03:00 +0300

    docs: rename example files to example prefix

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -8,12 +8,12 @@ repo_root = Path(__file__).parent.parent
 readme = (repo_root / "README.md").read_text()
 
 example_files_expected_to_appear_in_readme = [
-    repo_root / "examples/simple_server/server.py",
-    repo_root / "examples/simple_server/binary_request.sh",
-    repo_root / "examples/simple_server/structured_request.sh",
+    repo_root / "examples/simple_server/example_server.py",
+    repo_root / "examples/simple_server/example_binary_request.sh",
+    repo_root / "examples/simple_server/example_structured_request.sh",
     repo_root / "examples/simple_server/example_response.txt",
-    repo_root / "examples/binary_response_server/server.py",
-    repo_root / "examples/binary_response_server/request.sh",
+    repo_root / "examples/binary_response_server/example_server.py",
+    repo_root / "examples/binary_response_server/example_request.sh",
     repo_root / "examples/binary_response_server/example_response.txt",
 ]
 
