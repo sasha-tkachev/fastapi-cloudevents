b0793edd4b9b7ae80256a4d730fdd19344a6bd45
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 12:13:38 +0300

    refactor: extract json data fixing to function

--- fastapi_cloudevents/cloudevent_request.py
+++ fastapi_cloudevents/cloudevent_request.py
@@ -2,10 +2,27 @@ import json
 import re
 
 from cloudevents.conversion import to_dict, to_json
-from cloudevents.http import from_http
+from cloudevents.http import CloudEvent, from_http
 from starlette.requests import Request
 
 _JSON_CONTENT_TYPE = re.compile(r"^.+?/.*\+?json$", flags=re.IGNORECASE)
+
+
+def _should_fix_json_data_payload(event: CloudEvent):
+    if isinstance(event.data, (str, bytes)):
+        data_content_type = event.get("datacontenttype")
+        return data_content_type is None or _JSON_CONTENT_TYPE.match(data_content_type)
+    else:
+        return False  # not encoded json payload
+
+
+def _best_effort_fix_json_data_payload(event: CloudEvent) -> CloudEvent:
+    try:
+        if _should_fix_json_data_payload(event):
+            event.data = json.loads(event.data)
+    except (json.JSONDecodeError, TypeError, UnicodeDecodeError):
+        pass
+    return event
 
 
 class CloudEventRequest(Request):
@@ -14,15 +31,7 @@ class CloudEventRequest(Request):
             body = await super().body()
             if "ce-specversion" in self.headers:
                 event = from_http(dict(self.headers), body)
-                if isinstance(event.data, (str, bytes)):
-                    datacontenttype = event.get("datacontenttype")
-                    if datacontenttype is None or _JSON_CONTENT_TYPE.match(
-                        datacontenttype
-                    ):
-                        try:
-                            event.data = json.loads(event.data)
-                        except (json.JSONDecodeError, TypeError, UnicodeDecodeError):
-                            pass
+                event = _best_effort_fix_json_data_payload(event)
                 body = to_json(event)
                 self._json = to_dict(event)
             self._body = body
