42ac4ca0767fd1b4d2af41eb486b70d76a42f74f
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 15:25:57 +0300

    test: simple server

---
+++ tests/test_examples/test_simple_server.py
@@ -0,0 +1,50 @@
+import json
+
+import pytest
+from cloudevents.conversion import to_binary, to_structured
+from cloudevents.http import CloudEvent
+from starlette.testclient import TestClient
+
+from examples.simple_server.example_server import app
+
+
+@pytest.fixture()
+def client():
+    return TestClient(app)
+
+
+_DUMMY_SOURCE = "my-source"
+_DUMMY_TYPE = "my.event.v1"
+_DUMMY_JSON_DATA = {"a": "b"}
+_EXPECTED_HEADERS = {
+    "content-length",
+    "content-type",
+    "ce-specversion",
+    "ce-id",
+    "ce-source",
+    "ce-type",
+    "ce-time",
+}
+_EXPECTED_RESPONSE_HEADER_VALUES = {
+    "content-length": str(len(json.dumps(_DUMMY_JSON_DATA))),
+    "content-type": "application/json",
+    "ce-specversion": "1.0",
+    "ce-source": "http://testserver/",
+    "ce-type": "my.response-type.v1",
+}
+
+
+@pytest.mark.parametrize("to_http", (to_binary, to_structured))
+def test_binary_request_is_in_binary_format(client, to_http):
+    headers, data = to_http(
+        CloudEvent({"type": _DUMMY_TYPE, "source": _DUMMY_SOURCE}, _DUMMY_JSON_DATA)
+    )
+    response = client.post("/", headers=headers, data=data)
+    assert response.status_code == 200
+    assert set(response.headers.keys()) == _EXPECTED_HEADERS
+    assert {
+        k: v
+        for k, v in response.headers.items()
+        if k in _EXPECTED_RESPONSE_HEADER_VALUES
+    } == _EXPECTED_RESPONSE_HEADER_VALUES
+    assert response.json() == {"a": "b"}
