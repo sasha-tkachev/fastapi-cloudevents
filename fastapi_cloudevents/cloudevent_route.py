cd27cc8511cb7d391faa67e301e28fa15a6acc5b
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 04:09:31 +0300

    feat: route source tags

--- fastapi_cloudevents/cloudevent_route.py
+++ fastapi_cloudevents/cloudevent_route.py
@@ -6,6 +6,24 @@ from starlette.responses import Response
 
 from fastapi_cloudevents.cloudevent_request import CloudEventRequest
 from fastapi_cloudevents.cloudevent_response import _CloudEventResponse
+import re
+
+_CE_SOURCE_TAG_PREFIX = re.compile(r"^ce-source:", flags=re.IGNORECASE)
+
+
+def _is_source_tag(tag: str):
+    return _CE_SOURCE_TAG_PREFIX.match(tag)
+
+
+def _source_tag_to_source(source_tag: str) -> str:
+    return _CE_SOURCE_TAG_PREFIX.sub("", source_tag)
+
+
+def _route_source(route: APIRoute, request: Request) -> str:
+    try:
+        return next(map(_source_tag_to_source, filter(_is_source_tag, route.tags)))
+    except StopIteration:
+        return str(request.url)
 
 
 class CloudEventRoute(APIRoute):
@@ -17,7 +35,7 @@ class CloudEventRoute(APIRoute):
                 CloudEventRequest(request.scope, request.receive)
             )
             if isinstance(response, _CloudEventResponse):
-                response.replace_default_source(new_source=str(request.url))
+                response.replace_default_source(new_source=_route_source(self, request))
             return response
 
         return custom_route_handler
