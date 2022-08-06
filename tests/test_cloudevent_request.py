3e722ca32c60fb7789cb83f8f318d626f0a6049d
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 12:45:49 +0300

    test: _should_fix_json_data_payload

--- tests/test_cloudevent_request.py
+++ tests/test_cloudevent_request.py
@@ -1,6 +1,10 @@
 import pytest
+from cloudevents.http import CloudEvent
 
-from fastapi_cloudevents.cloudevent_request import _is_json_content_type
+from fastapi_cloudevents.cloudevent_request import (
+    _is_json_content_type,
+    _should_fix_json_data_payload,
+)
 
 
 @pytest.mark.parametrize(
@@ -23,5 +27,59 @@ from fastapi_cloudevents.cloudevent_request import _is_json_content_type
         ("/json", True),
     ],
 )
-def test_is_json_content_type_must_match_golden_samples(given, expected):
+def test_is_json_content_type_matches_golden_samples(given, expected):
     assert _is_json_content_type(given) == expected
+
+
+@pytest.mark.parametrize(
+    "given, expected",
+    [
+        (CloudEvent(attributes={"source": "a", "type": "a"}, data="{}"), True),
+        (
+            CloudEvent(
+                attributes={
+                    "source": "a",
+                    "type": "a",
+                    "datacontenttype": "application/json",
+                },
+                data="{}",
+            ),
+            True,
+        ),
+        (
+            CloudEvent(
+                attributes={
+                    "source": "a",
+                    "type": "a",
+                    "datacontenttype": "plain/text",
+                },
+                data="{}",
+            ),
+            False,
+        ),
+        (
+            CloudEvent(
+                attributes={
+                    "source": "a",
+                    "type": "a",
+                    "datacontenttype": "application/json",
+                },
+                data={},
+            ),
+            False,
+        ),
+        (
+            CloudEvent(
+                attributes={
+                    "source": "a",
+                    "type": "a",
+                    "datacontenttype": "plain/text",
+                },
+                data={},
+            ),
+            False,
+        ),
+    ],
+)
+def test_should_fix_payload_matches_golden_sample(given, expected):
+    assert _should_fix_json_data_payload(given) == expected
