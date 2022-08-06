23f74fb3912bc2c4be5d8acdf585a7194eaa2ccf
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 13:26:38 +0300

    test: update headers

--- tests/test_cloudevent_response.py
+++ tests/test_cloudevent_response.py
@@ -1,4 +1,12 @@
-from fastapi_cloudevents.cloudevent_response import _encoded_string
+from typing import AnyStr, Dict
+
+import pytest
+
+from fastapi_cloudevents.cloudevent_response import (
+    RawHeaders,
+    _encoded_string,
+    _update_headers,
+)
 
 
 def test_bytes_is_already_encoded():
@@ -8,3 +16,26 @@ def test_bytes_is_already_encoded():
 
 def test_str_should_be_encoded_in_utf_8():
     assert _encoded_string("Hello World") == b"Hello World"
+
+
+@pytest.mark.parametrize(
+    "given_headers, new_headers, expected",
+    [
+        (
+            [(b"ce-source", b"my-source"), (b"content-type", b"application/json")],
+            {"ce-source": "your-source"},
+            [(b"ce-source", b"your-source"), (b"content-type", b"application/json")],
+        ),
+        (
+            [(b"ce-source", b"my-source"), (b"content-type", b"application/json")],
+            {"Content-Type": b"plain/text"},
+            [(b"ce-source", b"my-source"), (b"content-type", b"plain/text")],
+        ),
+    ],
+)
+def test_update_headers_match_golden_sample(
+    given_headers: RawHeaders, new_headers: Dict[AnyStr, AnyStr], expected: RawHeaders
+):
+    result = _update_headers(given_headers, new_headers)
+    for value in expected:  # we cannot test order
+        assert value in result
