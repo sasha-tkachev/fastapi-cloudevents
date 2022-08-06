7fa3fadde1b461e6731cf28d75ed70b19939953d
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 18:00:23 +0300

    Revert "Revert "feat: add feature flag for creating events on behalf of the client""
    This reverts commit 08e4ae66dfd16067edd767828d5aa61668330b06.
    
    # Conflicts:
    #	fastapi_cloudevents/settings.py

--- fastapi_cloudevents/settings.py
+++ fastapi_cloudevents/settings.py
@@ -13,3 +13,4 @@ class ResponseMode(Enum):
 class CloudEventSettings(BaseSettings):
     default_source: Optional[constr(min_length=1)]
     default_response_mode: ResponseMode = ResponseMode.binary
+    create_events_on_behalf_of_the_client: bool = False
