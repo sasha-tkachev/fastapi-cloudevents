19b0f246bd59ea92e93f99f7565c6b808b27142b
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 12:47:39 +0300

    test: corrupt json deserialization

--- tests/test_cloudevent_request.py
+++ tests/test_cloudevent_request.py
@@ -2,9 +2,8 @@ import pytest
 from cloudevents.http import CloudEvent
 
 from fastapi_cloudevents.cloudevent_request import (
-    _is_json_content_type,
-    _should_fix_json_data_payload,
-)
+    _best_effort_fix_json_data_payload, _is_json_content_type,
+    _should_fix_json_data_payload)
 
 
 @pytest.mark.parametrize(
@@ -83,3 +82,13 @@ def test_is_json_content_type_matches_golden_samples(given, expected):
 )
 def test_should_fix_payload_matches_golden_sample(given, expected):
     assert _should_fix_json_data_payload(given) == expected
+
+
+def test_best_effort_event_fixing_should_not_fail_on_invalid_json():
+    corrupt_json = "{"
+    assert (
+        _best_effort_fix_json_data_payload(
+            CloudEvent(attributes={"source": "a", "type": "a"}, data=corrupt_json)
+        ).data
+        == corrupt_json
+    )
