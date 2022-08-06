d4dc49ed4659a7a398f42cb6ad957cdbcc4b5382
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 13:44:45 +0300

    test: structured event source replacement

--- tests/test_cloudevent_response.py
+++ tests/test_cloudevent_response.py
@@ -1,8 +1,10 @@
+import json
 from typing import AnyStr, Dict
 
 import pytest
 
 from fastapi_cloudevents import StructuredCloudEventResponse
+from fastapi_cloudevents.cloudevent import DEFAULT_SOURCE
 from fastapi_cloudevents.cloudevent_response import (RawHeaders,
                                                      _encoded_string,
                                                      _update_headers)
@@ -44,3 +46,50 @@ def test_re_rendering_structured_response_should_update_content_length():
     old_content_length = dict(response.raw_headers)[b"content-length"]
     response._re_render({"a": "b", "c": "d"})
     assert dict(response.raw_headers)[b"content-length"] != old_content_length
+
+
+@pytest.mark.parametrize(
+    "given, expected_body, expected_headers",
+    [
+        pytest.param(
+            StructuredCloudEventResponse({"source": "non-default-source"}),
+            {"source": "non-default-source"},
+            [
+                (b"content-length", b"31"),
+                (b"content-type", b"application/cloudevents+json"),
+            ],
+            id="non default source must not be replaced",
+        ),
+        pytest.param(
+            StructuredCloudEventResponse({"source": DEFAULT_SOURCE}),
+            {"source": "new-source"},
+            [
+                (b"content-length", b"23"),
+                (b"content-type", b"application/cloudevents+json"),
+            ],
+            id="default source should be replaced",
+        ),
+        pytest.param(
+            StructuredCloudEventResponse(
+                {"source": "dummy" + DEFAULT_SOURCE + "another"}
+            ),
+            {"source": "dummyfastapianother"},
+            [
+                (b"content-length", b"32"),
+                (b"content-type", b"application/cloudevents+json"),
+            ],
+            id=(
+                "sources which contain the default source as a sub string must not be "
+                "affected"
+            ),
+        ),
+    ],
+)
+def test_structured_replace_default_source_matches_golden_sample(
+    given: StructuredCloudEventResponse,
+    expected_body: Dict[str, str],
+    expected_headers: RawHeaders,
+):
+    given.replace_default_source("new-source")
+    assert json.loads(given.body) == expected_body
+    assert set(given.raw_headers) == set(expected_headers)
