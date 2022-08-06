08e4ae66dfd16067edd767828d5aa61668330b06
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 17:13:42 +0300

    Revert "feat: add feature flag for creating events on behalf of the client"
    This reverts commit b58bf95d305af446e30b127032043f9192ef8140.

--- fastapi_cloudevents/settings.py
+++ fastapi_cloudevents/settings.py
@@ -6,4 +6,3 @@ from pydantic.types import constr
 
 class CloudEventSettings(BaseSettings):
     default_source: Optional[constr(min_length=1)]
-    create_events_on_behalf_of_the_client: bool = False
