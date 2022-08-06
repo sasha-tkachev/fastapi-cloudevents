24587f3737d4a5315fdff87ab186e580ec920016
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-06 12:37:06 +0300

    test: is json content type

---
+++ tests/test_cloudevent_request.py
@@ -0,0 +1,27 @@
+import pytest
+
+from fastapi_cloudevents.cloudevent_request import _is_json_content_type
+
+
+@pytest.mark.parametrize(
+    "given, expected",
+    [
+        (None, True),
+        ("application/json", True),
+        ("application/xml", False),
+        ("json", False),
+        ("plain/text", False),
+        ("plain/json", True),
+        ("application/something+json", True),
+        ("application/cloudevents+json", True),
+        ("dummy/json", True),
+        ("dummy/dummy+json", True),
+        ("plain/dummy+json", True),
+        ("/dummy+json", True),
+        ("dummy/+json", True),
+        ("/+json", True),
+        ("/json", True),
+    ],
+)
+def test_is_json_content_type_must_match_golden_samples(given, expected):
+    assert _is_json_content_type(given) == expected
