31b48db540d2e2543422ab0ebf7648c4d6f3170d
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-20 01:03:43 +0300

    fix: add main check in setup.py

--- setup.py
+++ setup.py
@@ -10,40 +10,41 @@ def _read_requirements(filename):
     ]
 
 
-setuptools.setup(
-    name="fastapi-cloudevents",
-    version="1.0.0",
-    author="Alexander Tkachev",
-    author_email="sasha64sasha@gmail.com",
-    description="FastAPI plugin for CloudEvents Integration",
-    url="https://github.com/sasha-tkachev/fastapi-cloudevents",
-    keywords=[
-        "fastapi",
-        "cloudevents",
-        "ce",
-        "cloud",
-        "events",
-        "event",
-        "middleware",
-        "rest",
-        "rest-api",
-        "plugin",
-        "pydantic",
-        "fastapi-extension"
-    ],
-    packages=setuptools.find_packages(),
-    install_requires=_read_requirements("requirements.txt"),
-    classifiers=[
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: System Administrators",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: Apache Software License",
-        "Development Status :: 5 - Production/Stable",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-    ],
-)
+if __name__ == '__main__':
+    setuptools.setup(
+        name="fastapi-cloudevents",
+        version="1.0.0",
+        author="Alexander Tkachev",
+        author_email="sasha64sasha@gmail.com",
+        description="FastAPI plugin for CloudEvents Integration",
+        url="https://github.com/sasha-tkachev/fastapi-cloudevents",
+        keywords=[
+            "fastapi",
+            "cloudevents",
+            "ce",
+            "cloud",
+            "events",
+            "event",
+            "middleware",
+            "rest",
+            "rest-api",
+            "plugin",
+            "pydantic",
+            "fastapi-extension"
+        ],
+        packages=setuptools.find_packages(),
+        install_requires=_read_requirements("requirements.txt"),
+        classifiers=[
+            "Intended Audience :: Information Technology",
+            "Intended Audience :: System Administrators",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache Software License",
+            "Development Status :: 5 - Production/Stable",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
+            "Programming Language :: Python :: 3.10",
+        ],
+    )
