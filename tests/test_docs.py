dfde3feede1c3a00944d1c0a0881db5e921642b7
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-27 00:56:55 +0300

    test: coverage badge is correct

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import Path
 
 import pytest
@@ -6,6 +7,7 @@ repo_root = Path(__file__).parent.parent
 
 
 readme = (repo_root / "README.md").read_text()
+tox_config = (repo_root / "tox.ini").read_text()
 
 example_files_expected_to_appear_in_readme = [
     repo_root / "examples/simple_server/example_server.py",
@@ -28,3 +30,20 @@ example_files_expected_to_appear_in_readme = [
 )
 def test_certain_examples_should_appear_in_readme(example_file: str):
     assert example_file in readme
+
+
+def test_coverage_badge_must_represent_correct_coverage():
+    enforced_coverage = int(
+        re.search(r"--cov-fail-under=(?P<percent>[0-9]+)", tox_config).groupdict()[
+            "percent"
+        ]
+    )
+
+    badge_coverage = int(
+        re.search(
+            r"https://img.shields.io/badge/"
+            r"coverage-(?P<percent>[0-9]+)%25-brightgreen",
+            readme,
+        ).groupdict()["percent"]
+    )
+    assert badge_coverage <= enforced_coverage
