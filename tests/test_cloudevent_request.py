af30c586f645ab5ce40fb2c178e7c37b3c0975c0
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-27 00:43:41 +0300

    test: invalid cloudevents request

--- tests/test_cloudevent_request.py
+++ tests/test_cloudevent_request.py
@@ -1,10 +1,12 @@
+from typing import AsyncGenerator
+
 import pytest
+from cloudevents.exceptions import MissingRequiredFields
 from cloudevents.http import CloudEvent
 
+from fastapi_cloudevents import CloudEventRequest, CloudEventSettings
 from fastapi_cloudevents.cloudevent_request import (
-    _best_effort_fix_json_data_payload,
-    _should_fix_json_data_payload,
-)
+    _best_effort_fix_json_data_payload, _should_fix_json_data_payload)
 
 
 @pytest.mark.parametrize(
@@ -75,3 +77,21 @@ def test_best_effort_event_fixing_should_fix_valid_json():
     assert _best_effort_fix_json_data_payload(
         CloudEvent(attributes={"source": "a", "type": "a"}, data='{"hello": "world"}')
     ).data == {"hello": "world"}
+
+
+class FakeInvalidCloudEventRequest(CloudEventRequest):
+    def __init__(self):
+        super(FakeInvalidCloudEventRequest, self).__init__(
+            {"type": "http", "headers": []}
+        )
+
+    async def stream(self) -> AsyncGenerator[bytes, None]:
+        yield b'{"a": "b"}'
+
+
+@pytest.mark.asyncio
+async def test_when_disallowed_body_access_of_invalid_cloudevent_request_must_fail():
+    with pytest.raises(MissingRequiredFields):
+        await FakeInvalidCloudEventRequest.configured(
+            CloudEventSettings(allow_non_cloudevent_models=False)
+        )().body()
