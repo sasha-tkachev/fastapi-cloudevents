9088e9bfa92fb280b8557db6f2178096956fe8f3
Alexander Tkachev <sasha64sasha@gmail.com>
2022-08-27 00:23:19 +0300

    test: invalid default response mode

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,10 +1,19 @@
+import pytest
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
-from fastapi_cloudevents import install_fastapi_cloudevents
+from fastapi_cloudevents import CloudEventSettings, install_fastapi_cloudevents
+from fastapi_cloudevents.installation import _choose_default_response_class
 
 
 def test_user_warned_when_overriding_default_response_object(caplog):
     app = FastAPI(default_response_class=ORJSONResponse)
     install_fastapi_cloudevents(app)
     assert "WARNING" in caplog.text
+
+
+def test_choose_default_response_class_invalid_option_must_fail():
+    settings = CloudEventSettings()
+    settings.default_response_mode = object()
+    with pytest.raises(ValueError):
+        _choose_default_response_class(settings)
