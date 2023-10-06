ca883ecac3aa1d784b3d7a25589febc128d60b48
N. Ben Cohen <breathevalue@icloud.com>
2023-10-06 16:03:50 -0600

    Make changes to be compatible with pydantic v2

--- fastapi_cloudevents/settings.py
+++ fastapi_cloudevents/settings.py
@@ -1,8 +1,10 @@
 from enum import Enum
 from typing import Optional
 
-from pydantic import BaseSettings, Field
-from pydantic.types import constr
+from pydantic import StringConstraints, Field
+
+from typing_extensions import Annotated
+from pydantic_settings import BaseSettings
 
 
 class ContentMode(Enum):
@@ -11,7 +13,7 @@ class ContentMode(Enum):
 
 
 class CloudEventSettings(BaseSettings):
-    default_source: Optional[constr(min_length=1)]
+    default_source: Optional[Annotated[str, StringConstraints(min_length=1)]] = None
     default_response_mode: ContentMode = ContentMode.binary
     allow_non_cloudevent_models: bool = Field(
         default=True,
